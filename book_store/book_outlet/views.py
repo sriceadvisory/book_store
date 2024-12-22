f204149000d6d5d1e4bd2c8f016cd20ae1ff6372
srice2024 <srice@legacyforcemedia.com>
2024-12-21 17:02:15 -0800

    Update - Adding in logic and dynamic urls.

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Book
+from django.http import Http404
 
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+    
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
+    })
